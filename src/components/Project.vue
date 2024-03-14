d06f02a827c62d717a3aa08ba66eeef39ff9f632
Jared Garcia <jaredgarcia@MacBook-Pro-de-Jared.local>
2024-03-13 18:04:23 -0600

    code-page from projects

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="bg-white rounded-xl shadow-xl w-80">
+  <div
+    class="bg-white rounded-xl shadow-xl w-80 relative min-h-[490px] md:min-h-[560px]"
+  >
     <img :src="project.image" alt="" class="rounded-t-xl" />
     <div class="px-5 pb-5 md:px-9 md:pb-9">
       <div class="my-3 flex">
@@ -17,9 +19,23 @@
           {{ project.description }}
         </p>
       </div>
-      <div class="flex justify-end mt-4">
-        <ButtonMain
-          ><svg
+      <div class="flex justify-end mt-4 gap-4 absolute bottom-5 right-5">
+        <a
+          :href="project.page"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="border-2 py-2 px-4 rounded-lg text-sm md:text-2xl text-gray-400 hover:border-none hover:bg-green-500 hover:font-bold hover:text-white transition-all flex justify-center items-center"
+        >
+          Visit
+        </a>
+        <a
+          v-if="project.code"
+          :href="project.code"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="border-2 py-2 px-4 rounded-lg text-sm md:text-2xl text-gray-400 hover:border-none hover:bg-green-500 hover:font-bold hover:text-white transition-all flex items-center"
+          >Code
+          <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
@@ -34,8 +50,9 @@
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M7 8l-4 4l4 4" />
             <path d="M17 8l4 4l-4 4" />
-            <path d="M14 4l-4 16" /></svg
-        ></ButtonMain>
+            <path d="M14 4l-4 16" />
+          </svg>
+        </a>
       </div>
     </div>
   </div>
